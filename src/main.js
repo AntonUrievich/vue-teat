66964a92a0871885342f051c07005526efd801d0
AntonUrievich <AntonUrievi4@gmail.com>
2021-09-03 19:59:18 +0400

    firebase

--- src/main.js
+++ src/main.js
@@ -6,5 +6,20 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
-createApp(App).use(store).use(router).use (Vuelidate (required, email)).mount('#app')
+createApp(App).use(store).use(router).use(Vuelidate (required, email)).use(firebaseConfig).use(analytics).mount('#app')
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAg6MIiEv0fEztNUtE07zI__FF-OqQHV1c",
+    authDomain: "vue-test-9bc56.firebaseapp.com",
+    projectId: "vue-test-9bc56",
+    storageBucket: "vue-test-9bc56.appspot.com",
+    messagingSenderId: "954379291165",
+    appId: "1:954379291165:web:0d5ca0983f1932454014f4",
+    measurementId: "G-MXD8XEKX1C"
+  };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
